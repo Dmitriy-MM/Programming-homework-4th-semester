659d39a6d9460e06b720697badf5631dbee361aa
Dmitriy-MM <dimapavlov-2015@mail.ru>
2021-02-27 00:01:42 +0300

    Update to_test.sh

--- Task02 2021_02_22 regexp/to_test.sh
+++ Task02 2021_02_22 regexp/to_test.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-echo "making"
-make clean > __tmp.txt
-make > __tmp.txt
-rm -f __tmp.txt
-echo "making end"
-#cat $1 > dimas_summary.txt
-#sed -i -e '/^\s*$/e' -e 's/Elapsed = [0-9]*.[0-9]*//' -e '/g++/d' -e 's/\s\+/ /' -e '/^[a-zA-Z 0-9]*$/d'  -e 's/^.*\([/\\]*.*\.out \)/&/g' dimas_summary.txt
-echo "start test (my_summary.txt and dimas_summary.txt)"
-date +%c
-make test > my_summary.txt
-date +%c
-sed -i -e '/^\s*$/e' -e 's/Elapsed = [0-9]*.[0-9]*//' -e '/g++/d' -e 's/\s\+/ /' -e '/^[a-zA-Z 0-9]*$/d'  -e 's/^.*\([/\\]*.*\.out \)/&/g' my_summary.txt
-./to_check_only.sh $1
-echo "end test"
+if [ -f "$1" ]
+then
+	echo "making"
+	make clean > __tmp.txt
+	make > __tmp.txt
+	rm -f __tmp.txt
+	echo "making end"
+	#cat $1 > dimas_summary.txt
+	#sed -i -e '/^\s*$/e' -e 's/Elapsed = [0-9]*.[0-9]*//' -e '/g++/d' -e 's/\s\+/ /' -e '/^[a-zA-Z 0-9]*$/d'  -e 's/^.*\([/\\]*.*\.out \)/&/g' dimas_summary.txt
+	echo "start test (my_summary.txt and dimas_summary.txt)"
+	date +%c
+	make test > my_summary.txt
+	date +%c
+	sed -i -e '/^\s*$/e' -e 's/Elapsed = [0-9]*.[0-9]*//' -e '/g++/d' -e 's/\s\+/ /' -e '/^[a-zA-Z 0-9]*$/d'  -e 's/^.*\([/\\]*.*\.out \)/&/g' my_summary.txt
+	./to_check_only.sh $1
+	echo "end test"
+else
+	echo "Usage: $0 results_filename.txt"
+	exit [1]
+fi
