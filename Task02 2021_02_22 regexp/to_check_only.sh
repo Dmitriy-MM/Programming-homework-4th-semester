95ee59de714ed19489b5d2df4d9758a3475f50eb
Dmitriy-MM <dimapavlov-2015@mail.ru>
2021-02-26 19:29:46 +0300

    Update to_check_only.sh
    Corrected cutting programm-name

--- Task02 2021_02_22 regexp/to_check_only.sh
+++ Task02 2021_02_22 regexp/to_check_only.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cat $1 > dimas_summary.txt
-sed -i -e '/^\s*$/e' -e 's/Elapsed = [0-9]*.[0-9]*//' -e '/g++/d' -e 's/\s\+/ /' -e '/^[a-zA-Z 0-9]*$/d'  -e 's/^.*\([/\\]*.*\.out \)/&/g' dimas_summary.txt
-sed -i -e '/^\s*$/e' -e 's/Elapsed = [0-9]*.[0-9]*//' -e '/g++/d' -e 's/\s\+/ /' -e '/^[a-zA-Z 0-9]*$/d'  -e 's/^.*\([/\\]*.*\.out \)/&/g' my_summary.txt
+sed -i -e '/^\s*$/e' -e 's/Elapsed = [0-9]*.[0-9]*//' -e '/g++/d' -e 's/\s\+/ /' -e '/^[a-zA-Z 0-9]*$/d'  -e 's/^.*\(a[0-9]\+\.out\)/\1/g' dimas_summary.txt
+sed -i -e '/^\s*$/e' -e 's/Elapsed = [0-9]*.[0-9]*//' -e '/g++/d' -e 's/\s\+/ /' -e '/^[a-zA-Z 0-9]*$/d'  -e 's/^.*\(a[0-9]\+\.out\)/\1/g' my_summary.txt
 diff -q -s my_summary.txt dimas_summary.txt
 diff -y my_summary.txt dimas_summary.txt
 diff -q -s my_summary.txt dimas_summary.txt
+
